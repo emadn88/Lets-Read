178e90ef392af5aa155a8919630ea2d3c6801e49
emad360 <84744963+emad360@users.noreply.github.com>
2022-05-29 23:15:13 +0300

    sync sound with "هَلْ تَسْتَطِيعُ أَنْ تَحْزِرَ الاِسْمَ؟"

--- Lets Read/Controller/Story Controller/ST12ViewController.swift
+++ Lets Read/Controller/Story Controller/ST12ViewController.swift
@@ -5,25 +5,74 @@
 //  Created by Emad Abd on 28/05/2022.
 //
 
+ 
+
 import UIKit
+import AVFoundation
 
-class ST12ViewController: UIViewController {
 
+class ST12ViewController: UIViewController , AVSpeechSynthesizerDelegate {
+    
+    var player: AVAudioPlayer?
+
+    @IBOutlet var lblText: UILabel!
+    
+    var speechSynthesizer = AVSpeechSynthesizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        speechSynthesizer.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+//
+        Timer.scheduledTimer(timeInterval: 0.3,
+            target: self,
+            selector: #selector(ST12ViewController.playSound2),
+            userInfo: nil,
+            repeats: false)
 
-        // Do any additional setup after loading the view.
+        let string = "هَلْ تَسْتَطِيعُ أَنْ تَحْزِرَ الاِسْمَ؟"
+        let utterance = AVSpeechUtterance(string: string)
+//        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Ting-Ting-compact")
+        utterance.rate = 0.45
+        utterance.pitchMultiplier = 1.3
+        utterance.volume = 0.0
+        speechSynthesizer.speak(utterance)
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        let mutableAttributedString = NSMutableAttributedString(string: utterance.speechString)
+        mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.yellow, range: characterRange)
+        lblText.attributedText = mutableAttributedString
     }
-    */
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        lblText.attributedText = NSAttributedString(string: utterance.speechString)
+    }
+    
+    
+    
+    @objc  func playSound2() {
+        guard let url = Bundle.main.url(forResource: "knowName", withExtension: "mp3") else { return }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
+
--- Lets Read/Controller/TsetViewController.swift
+++ Lets Read/Controller/TsetViewController.swift
@@ -1,31 +1,49 @@
 //
-//  ST13ViewController.swift
+//  TsetViewController.swift
 //  Lets Read
 //
-//  Created by Emad Abd on 28/05/2022.
+//  Created by Emad Abd on 29/05/2022.
 //
 
 import UIKit
 import AVFoundation
 
-class ST13ViewController: UIViewController   , AVSpeechSynthesizerDelegate {
-    var player: AVAudioPlayer?
-    var speechSynthesizer = AVSpeechSynthesizer()
-    @IBOutlet var lblText: UILabel!
 
+class TsetViewController: UIViewController , AVSpeechSynthesizerDelegate {
+    
+    var player: AVAudioPlayer?
+
+    @IBOutlet var lblText: UILabel!
+    
+    var speechSynthesizer = AVSpeechSynthesizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Timer.scheduledTimer(timeInterval: 1,
+        speechSynthesizer.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+//
+        Timer.scheduledTimer(timeInterval: 0.3,
             target: self,
-            selector: #selector(ST13ViewController.changeTxt),
+            selector: #selector(TsetViewController.playSound2),
             userInfo: nil,
             repeats: false)
-        
-        
 
+     
         
-     }
+        
+//        ()
+
+        let string =
+        "شَعَرَتِ الفَتَاةُ الصَّغِيرَةُ بِالتَّعَبِ  بَعْدَ تِلْكَ الرِّحْلَةِ الشَّاقَّةِ.  يَالَهَا مِنْ فَتَاةٍ رَائِعَةٍ وَشُجَاعَةٍ تُــدْعَــى..."
+        let utterance = AVSpeechUtterance(string: string)
+//        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Ting-Ting-compact")
+        utterance.rate = 0.45
+        utterance.pitchMultiplier = 1.3
+        utterance.volume = 0.0
+        speechSynthesizer.speak(utterance)
+        
+        
+    }
     
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
         let mutableAttributedString = NSMutableAttributedString(string: utterance.speechString)
@@ -37,19 +55,21 @@ class ST13ViewController: UIViewController   , AVSpeechSynthesizerDelegate {
         lblText.attributedText = NSAttributedString(string: utterance.speechString)
     }
     
- 
-    @objc func changeTxt(){
-        
-        
-        let string =  "هَلْ تَسْتَطِيعُ أَنْ تَحْزِرَ الاِسْمَ؟"
+    @IBAction func startPressed(_ sender: Any) {
+        playSound2()
+
+        let string =  "فِي صَبَاحِ أَحَدِ الأيَّامِ، اِسْتَيْقَظَتْ فَتَاةٌ صَغِيرَةٌ مِنْ نَوْمِهَا"
         let utterance = AVSpeechUtterance(string: string)
 //        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.Ting-Ting-compact")
-        utterance.rate = 0.45
+        utterance.rate = 0.35
         utterance.pitchMultiplier = 1.3
         utterance.volume = 0.0
         speechSynthesizer.speak(utterance)
-        
-        
+    }
+    
+    
+    
+    @objc  func playSound2() {
         guard let url = Bundle.main.url(forResource: "knowName", withExtension: "mp3") else { return }
 
         do {
@@ -69,9 +89,6 @@ class ST13ViewController: UIViewController   , AVSpeechSynthesizerDelegate {
         } catch let error {
             print(error.localizedDescription)
         }
-
-        
     }
-    
-
 }
+
